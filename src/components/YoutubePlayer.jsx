0c41100642ff87d101ad8469e12cc340db92b468
Siddhant <siddhantrsen73938@gmail.com>
2023-10-08 15:31:06 +0530

    Minor changes

--- src/components/YoutubePlayer.jsx
+++ src/components/YoutubePlayer.jsx
@@ -7,9 +7,8 @@ const YoutubePlayer = ({ url, para }) => {
       <p className='w-[390px] px-2 py-4'>{para}</p>
       {
         url === 'https://www.youtube.com/embed/oZMzRjHqQCI?si=A1STtB0lrP2Ex3SU' && <p className='w-[390px] px-2 py-4' >
-
-          Play on “Public Safety of Women,” resulting in an audience of 250+ children and adults from <a href="https://www.chaitanyakul.in/" target="_blank" rel="noopener noreferrer" className='font-quicksand font-[400] text-[18px] text-purple underline '>Chaitanya Kul</a> and <a href="https://www.thinksharpfoundation.org/" target="_blank" rel="noopener noreferrer" className='font-quicksand font-[400] text-[18px] text-purple underline '>ThinkSharp Foundation</a>. <br /><br />
-          Featured: <a href="https://tribesforgood.com/storytelling-for-social-impact-a-step-by-step-guide-to-creating-powerful-plays/" target="_blank" rel="noopener noreferrer" className='font-quicksand font-[400] text-[18px] text-purple underline '>Blog1</a>, <a href="https://tribesforgood.com/empowering-social-discourse-araynas-journey-through-theatre-and-activism/" target="_blank" rel="noopener noreferrer" className='font-quicksand font-[400] text-[18px] text-purple underline '>Blog 2</a>
+          Play on “Public Safety of Women,” resulting in an audience of 250+ children and adults from <a href="https://www.chaitanyakul.in/" target="_blank" rel="noopener noreferrer" className='font-quicksand font-[400] text-[16px] text-purple underline '>Chaitanya Kul</a> and <a href="https://www.thinksharpfoundation.org/" target="_blank" rel="noopener noreferrer" className='font-quicksand font-[400] text-[16px] text-purple underline '>ThinkSharp Foundation</a>. <br /><br />
+          Featured: <a href="https://tribesforgood.com/storytelling-for-social-impact-a-step-by-step-guide-to-creating-powerful-plays/" target="_blank" rel="noopener noreferrer" className='font-quicksand font-[400] text-[16px] text-purple underline '>Blog1</a>, <a href="https://tribesforgood.com/empowering-social-discourse-araynas-journey-through-theatre-and-activism/" target="_blank" rel="noopener noreferrer" className='font-quicksand font-[400] text-[16px] text-purple underline '>Blog 2</a>
         </p>
       }
     </div>

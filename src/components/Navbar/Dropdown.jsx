1ffbd9586a06846b51e41f9793d60522a9a0c808
Siddhant <siddhantrsen73938@gmail.com>
2023-10-28 12:35:11 +0530

    New changes

--- src/components/Navbar/Dropdown.jsx
+++ src/components/Navbar/Dropdown.jsx
@@ -1,56 +1,121 @@
-import React, { useState } from "react"
-import { Link } from "react-router-dom";
+// import React, { useState } from "react"
+// import { Link } from "react-router-dom";
+
+// const sublinks = [
+//     {
+//         id: 1,
+//         text: 'bharatnatyam',
+//         url: 'bharatnatyam'
+//     },
+//     {
+//         id: 2,
+//         text: 'theatre',
+//         url: 'drama'
+//     },
+//     {
+//         id: 3,
+//         text: 'drums',
+//         url: 'drums'
+//     },
+//     {
+//         id: 4,
+//         text: 'piano',
+//         url: 'piano'
+//     },
+// ]
+
+// const Dropdown = () => {
+
+//     const [dropdown, setDropdown] = useState(false);
+
+//     return (
+//         <div>
+//             <ul
+//                 className='transition mt-4 absolute top-[64px] w-[120px] p-3 flex flex-col gap-2 z-30 shadow-md bg-gray/30'
+//                 onClick={() => setDropdown(!dropdown)}
+//             >
+//                 {sublinks.map((sublink) => {
+//                     const { id, text, url } = sublink
+//                     return (
+//                         <a
+//                             key={id}
+//                             href={`#${url}`}
+//                             className='font-raleway text-[15px] font-normal'
+//                             onClick={() => setDropdown(false)}
+//                         >
+//                             {text}
+//                         </a>
+//                     );
+//                 })}
+//             </ul>
+//         </div>
+//     )
+// }
+
+// export default Dropdown
+
+
+import React, { useState } from "react";
 
 const sublinks = [
-    {
-        id: 1,
-        text: 'bharatnatyam',
-        url: 'bharatnatyam'
-    },
-    {
-        id: 2,
-        text: 'theatre',
-        url: 'drama'
-    },
-    {
-        id: 3,
-        text: 'drums',
-        url: 'drums'
-    },
-    {
-        id: 4,
-        text: 'piano',
-        url: 'piano'
-    },
-]
+  {
+    id: 1,
+    text: 'Bharatnatyam',
+    sectionId: 'bharatnatyam',
+  },
+  {
+    id: 2,
+    text: 'Theatre',
+    sectionId: 'drama',
+  },
+  {
+    id: 3,
+    text: 'Drums',
+    sectionId: 'drums',
+  },
+  {
+    id: 4,
+    text: 'Piano',
+    sectionId: 'piano',
+  },
+];
 
 const Dropdown = () => {
+  const [dropdown, setDropdown] = useState(false);
 
-    const [dropdown, setDropdown] = useState(false);
+  // Function to handle smooth scrolling
+  function handleNavigation(sectionId) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 
-    return (
-        <div>
-            <ul
-                className='transition mt-4 absolute top-[64px] w-[120px] p-3 flex flex-col gap-2 z-30 shadow-md bg-gray/30'
-                onClick={() => setDropdown(!dropdown)}
+  return (
+    <div>
+      <ul
+        className='transition mt-4 absolute top-[64px] w-[120px] p-3 flex flex-col gap-2 z-30 shadow-md bg-gray/30'
+        onClick={() => setDropdown(!dropdown)}
+      >
+        {sublinks.map((sublink) => {
+          const { id, text, sectionId } = sublink;
+          return (
+            <a
+              key={id}
+              href={`/extracurricular#${sectionId}`}
+              className='font-raleway text-[15px] font-normal'
+              onClick={() => {
+                setDropdown(false);
+                handleNavigation(sectionId);
+              }}
             >
-                {sublinks.map((sublink) => {
-                    const { id, text, url } = sublink
-                    return (
-                        <li key={id}>
-                            <a
-                                href={`#${url}`}
-                                className='font-raleway text-[15px] font-normal'
-                                onClick={() => setDropdown(false)}
-                            >
-                                {text}
-                            </a>
-                        </li>
-                    );
-                })}
-            </ul>
-        </div>
-    )
+              {text}
+            </a>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
-export default Dropdown
+export default Dropdown;
--- src/pages/ExtraCurricularPage.jsx
+++ src/pages/ExtraCurricularPage.jsx
@@ -8,14 +8,30 @@ import Drums from './Drums'
 import Piano from './Piano'
 
 const ExtraCurricularPage = () => {
+
+  function handleNavigation(sectionId) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
   return (
     <section className='min-h-screen flex flex-col items-center justify-evenly'>
       <PageHero />
       <TalentSlider />
-      <Bharatnatyam />
-      <Drama />
-      <Drums />
-      <Piano />
+      <div id='bharatnatyam'>
+        <Bharatnatyam />
+      </div>
+      <div id="drama">
+        <Drama />
+      </div>
+      <div id="drums">
+        <Drums />
+      </div>
+      <div id="piano">
+        <Piano />
+      </div>
       {/* <Outlet /> */}
     </section>
   )
